c85cd4ed7794b359270021f0275509d9d5c42970
Anupat Insuvanno <rickgrade3@gmail.com>
2021-04-22 21:33:26 +0700

    www

--- packages/frontend/src/services/RP.tsx
+++ packages/frontend/src/services/RP.tsx
@@ -23,16 +23,51 @@ export class Hop {
     const configuration = {
       iceServers: [
         {
-          urls: "stun:stun.l.google.com:19302",
+          url: "turn:relay.backups.cz",
+          credential: "webrtc",
+          username: "webrtc",
+        },
+        {
+          url: "turn:relay.backups.cz?transport=tcp",
+          credential: "webrtc",
+          username: "webrtc",
+        },
+        {
+          urls: "stun.l.google.com:19302",
         },
         {
           urls: "turn:0.peerjs.com:3478",
           username: "peerjs",
           credential: "peerjsp",
         },
+        {
+          url: "turn:numb.viagenie.ca",
+          credential: "muazkh",
+          username: "webrtc@live.com",
+        },
+        {
+          url: "turn:192.158.29.39:3478?transport=udp",
+          credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+          username: "28224511:1379330808",
+        },
+        {
+          url: "turn:192.158.29.39:3478?transport=tcp",
+          credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+          username: "28224511:1379330808",
+        },
+        {
+          url: "turn:turn.bistri.com:80",
+          credential: "homeo",
+          username: "homeo",
+        },
+        {
+          url: "turn:turn.anyfirewall.com:443?transport=tcp",
+          credential: "webrtc",
+          username: "webrtc",
+        },
       ],
       sdpSemantics: "unified-plan",
-    };
+    } as any;
     this.pc = new RTCPeerConnection(configuration);
     if (!this.isOutgoing) {
       const audioEl = document.createElement("audio");
